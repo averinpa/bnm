00d066127bd01c26ffef90ddd083670e4ae88609
Pavel Averin <averinjr@gmail.com>
2025-04-01 16:50:18 +0300

    change long description in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="bnm",
     version="0.1.0",
     description="A package for evaluating and visualizing DAGs.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),   
     long_description_content_type="text/markdown",
     author="Pavel Averin",
     author_email="averinjr@gmail.com",
